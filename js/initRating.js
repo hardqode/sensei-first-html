50b1d5ec526828cc2c028a066375fe06f1d2a176
kuroedov_maxim <Ni7pfpNh>
2017-10-31 09:35:22 +0300

    init object card

--- js/initRating.js
+++ js/initRating.js
@@ -27,3 +27,31 @@ $('#example-fontawesome-o').barrating({
             .addClass('hidden');
     }
 });
+$('#example-fontawesome-e').barrating({
+    theme: 'fontawesome-stars-o',
+    showSelectedRating: false,
+    initialRating: '3.5',
+    readonly: true,
+    onSelect: function (value, text) {
+        if (!value) {
+            $('#example-fontawesome-e')
+                .barrating('clear');
+        } else {
+            $('.stars-example-fontawesome-e .current-rating')
+                .addClass('hidden');
+
+            $('.stars-example-fontawesome-e .your-rating')
+                .removeClass('hidden')
+                .find('span')
+                .html(value);
+        }
+    },
+    onClear: function (value, text) {
+        $('.stars-example-fontawesome-e')
+            .find('.current-rating')
+            .removeClass('hidden')
+            .end()
+            .find('.your-rating')
+            .addClass('hidden');
+    }
+});
